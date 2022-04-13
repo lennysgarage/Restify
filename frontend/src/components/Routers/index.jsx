d84bac21fc3beeb8c3fe7dae561fc13d302422c3
Jackson Kelly <jackson.kelly@mail.utoronto.ca>
2022-04-13 00:14:45 +0000

    added other routes

--- frontend/src/components/Routers/index.jsx
+++ frontend/src/components/Routers/index.jsx
@@ -6,14 +6,14 @@ const Router = () => {
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<LandingPage />}>
-                {/*<Route path="/register" element={<RegisterPage />}>
-                <Route path="/login" element={<LoginPage />}>
-                <Route path="/profile" element={<ProfilePage />}>
-                <Route path="/feed" element={<FeedPage />}>
-                <Route path="/search/?q=" element={<SearchResultsPage />}>
-                <Route path="/restaurant" element={<CreateRestaurantPage />}>
-                <Route path="/restaurant/<restaurantid>" element={<RestaurantPage />}>
-                <Route path="/restaurant/<restaurantid>/blog/<blogid>" element={<BlogPage />}>*/}
+                {/*<Route path="/register" element={<RegisterPage />}> </Route>
+                <Route path="/login" element={<LoginPage />}> </Route>
+                <Route path="/profile" element={<ProfilePage />}> </Route>
+                <Route path="/feed" element={<FeedPage />}> </Route>
+                <Route path="/search/?q=" element={<SearchResultsPage />}> </Route>
+                <Route path="/restaurant" element={<CreateRestaurantPage />}> </Route>
+                <Route path="/restaurant/<restaurantid>" element={<RestaurantPage />}> </Route>
+                <Route path="/restaurant/<restaurantid>/blog/<blogid>" element={<BlogPage />}> </Route>*/}
                 </Route>
             </Routes>
         </BrowserRouter>
