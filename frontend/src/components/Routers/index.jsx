6958835f8b6adb792a9e44bf7b1fef41d9789066
Jackson Kelly <jackson.kelly@mail.utoronto.ca>
2022-04-13 00:09:29 +0000

    added other routes

--- frontend/src/components/Routers/index.jsx
+++ frontend/src/components/Routers/index.jsx
@@ -6,7 +6,14 @@ const Router = () => {
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<LandingPage />}>
-                    {/* Add other routers here */ }
+                {/*<Route path="/register" element={<RegisterPage />}>
+                <Route path="/login" element={<LoginPage />}>
+                <Route path="/profile" element={<ProfilePage />}>
+                <Route path="/feed" element={<FeedPage />}>
+                <Route path="/search/?q=" element={<SearchResultsPage />}>
+                <Route path="/restaurant" element={<CreateRestaurantPage />}>
+                <Route path="/restaurant/<restaurantid>" element={<RestaurantPage />}>
+                <Route path="/restaurant/<restaurantid>/blog/<blogid>" element={<BlogPage />}>*/}
                 </Route>
             </Routes>
         </BrowserRouter>
