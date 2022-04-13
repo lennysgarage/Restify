70a7a2ffe875173d160d8976a664d22cce0fe769
Jackson Kelly <jackson.kelly@mail.utoronto.ca>
2022-04-13 00:32:55 +0000

    Added default pages

--- frontend/src/components/Routers/index.jsx
+++ frontend/src/components/Routers/index.jsx
@@ -1,4 +1,12 @@
 import LandingPage from "../../pages/LandingPage";
+import RegisterPage from "../../pages/RegisterPage";
+import LoginPage from "../../pages/LoginPage";
+import ProfilePage from "../../pages/ProfilePage";
+import FeedPage from "../../pages/FeedPage";
+import SearchResultsPage from "../../pages/SearchResultsPage";
+import CreateRestaurantPage from "../../pages/CreateRestaurantPage";
+import RestaurantPage from "../../pages/RestaurantrPage";
+import BlogPage from "../../pages/BlogPage";
 import { BrowserRouter , Routes, Route } from "react-router-dom";
 
 const Router = () => {
@@ -6,14 +14,14 @@ const Router = () => {
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<LandingPage />}>
-                {/*<Route path="register" element={<RegisterPage />}> />
-                <Route path="login" element={<LoginPage />}> />
-                <Route path="profile" element={<ProfilePage />}> />
-                <Route path="feed" element={<FeedPage />}> />
-                <Route path="search/?q=" element={<SearchResultsPage />}> />
-                <Route path="restaurant" element={<CreateRestaurantPage />}> />
-                <Route path="restaurant/<restaurantid>" element={<RestaurantPage />}> />
-                <Route path="restaurant/<restaurantid>/blog/<blogid>" element={<BlogPage />}> />*/}
+                <Route path="register" element={<RegisterPage />} />
+                <Route path="login" element={<LoginPage />} />
+                <Route path="profile" element={<ProfilePage />} />
+                <Route path="feed" element={<FeedPage />} />
+                <Route path="search/?q=" element={<SearchResultsPage />} />
+                <Route path="restaurant" element={<CreateRestaurantPage />} />
+                <Route path="restaurant/<restaurantid>" element={<RestaurantPage />} />
+                <Route path="restaurant/<restaurantid>/blog/<blogid>" element={<BlogPage />} />
                 </Route>
             </Routes>
         </BrowserRouter>
