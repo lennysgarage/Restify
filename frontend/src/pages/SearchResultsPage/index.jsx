9af11e528c8a71fb6e029c0106f42ba6638de986
marcan11 <lennysgarage1@gmail.com>
2022-04-17 00:25:58 -0400

    Basic search result item view

--- frontend/src/pages/SearchResultsPage/index.jsx
+++ frontend/src/pages/SearchResultsPage/index.jsx
@@ -1,24 +1,60 @@
 import React, {useState, useEffect} from 'react';
 import SearchBar from "../../components/Search/searchbarpage.jsx";
 import searchRequest from '../../components/Search/searchrequest';
+import Pagination from '@mui/material/Pagination';
+import Container from "@mui/material/Container";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
 
 export default function SearchResultsPage() {
 
     const [query, setQuery] = useState("");
+    const [page, setPage] = useState(1);
+    const [count, setCount] = useState(1);
+    const PER_PAGE = 1;
     const [data, setData] = useState(null);
 
     
+
     useEffect(() => {
         const search = () => {
-            searchRequest(query)
-              .then(response => {console.log(response.data)});
+            searchRequest(query + "&page=" + page)
+              .then(response => {
+                    console.log(response.data);
+                    setData(response.data.results);
+                    setCount(Math.ceil(response.data.count/PER_PAGE));
+                });
         }
         search();
-    }, [query]);
+    }, [query, page]);
+
+    const handleChange = (e, p) => {
+        setPage(p);
+    }
 
     return (
-        <SearchBar setQuery={setQuery}/>
-        // map through the data
+        // console.log(Object.entries(data)); 
+        <Container component="main" maxWidth="lg">
+            <SearchBar setQuery={setQuery}/>
+            {/* // map through the data */}
+            <List spacing={2}>
+                {data !== null && data.map(r => {
+                    return (
+                        <ListItem primaryText={r.name} key={r.id}> 
+                            <ListItemText primary={r.name} />
+                        </ListItem>
+                    );
+                })}
+            </List>
 
+            <Pagination
+                count={count}
+                page={page}
+                variant="outlined"
+                color="primary"
+                onChange={handleChange}
+            />
+        </Container>
     );
 }
