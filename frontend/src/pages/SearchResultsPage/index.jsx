304665cb48aa31fcbb882f17315b78410a1322fd
marcan11 <lennysgarage1@gmail.com>
2022-04-18 08:56:30 -0400

    Search page pagination sizing

--- frontend/src/pages/SearchResultsPage/index.jsx
+++ frontend/src/pages/SearchResultsPage/index.jsx
@@ -39,7 +39,7 @@ export default function SearchResultsPage() {
             {/* // map through the data */}
             <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
                 {data !== null && data.map((r, index) => (
-                    <Grid item xs={2} sm={4} md={4} key={index} sx={{ m: 2}}>
+                    <Grid item xs={12} sm={6} md={3} key={index} sx={{ m: 2}}>
                         <SearchResultCard info={r} />
                     </Grid>
                 ))}
