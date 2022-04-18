61b5412e6b7b4bcf3018002385a9a2c76a83d2d0
Jackson Kelly <jackson.kelly@mail.utoronto.ca>
2022-04-18 04:35:21 +0000

    Fixed addblog

--- frontend/src/pages/AddBlogPage/index.jsx
+++ frontend/src/pages/AddBlogPage/index.jsx
@@ -1,3 +1,4 @@
+import { Box, Container, Typography } from "@mui/material";
 import React from "react";
 import { useParams } from 'react-router-dom';
 import AddBlog from "../../components/AddBlog";
@@ -5,5 +6,22 @@ import AddBlog from "../../components/AddBlog";
 
 export default function AddBlogPage() {
     const { restaurant_id } = useParams();
-    return <AddBlog id={restaurant_id} />
+    return (
+        <Container component="main" maxWidth="sm">
+            <Box 
+                sx={{
+                marginTop: 8,
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+            }}
+            >
+                <Typography component="h1" variant="h5">
+                    Add a Blog Post
+                </Typography>
+                
+                <AddBlog id={restaurant_id} />
+            </Box>
+        </Container>
+    );
 }
