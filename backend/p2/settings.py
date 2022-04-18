f8c18745ce955a1955e9e4e53d0f3923e8902121
Jackson Kelly <jackson.kelly@mail.utoronto.ca>
2022-04-18 12:50:15 +0000

    Added database

--- backend/p2/settings.py
+++ backend/p2/settings.py
@@ -141,7 +141,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 6
 }
 
 
